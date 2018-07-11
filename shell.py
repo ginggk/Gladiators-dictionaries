6d29dfcb9fa56d114d3a24da01ff8d8ced6c140d
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-11 11:36:45 -0500

    gladiator game 4.0!

--- shell.py
+++ shell.py
@@ -10,6 +10,7 @@ def gladiator():
     print("*GLADIATOR 1*")
     while True:  # whole game play loop
         if core.is_dead(gladiator_1):
+            print("Gladiator 1 has died, always in our hearts.")
             print("~~~GLADIATOR 2 WINS~~~")
             break
 
@@ -24,19 +25,25 @@ def gladiator():
             ).strip().lower()
             if response == 'attack':
                 core.attack(gladiator_1, gladiator_2)
+                print("You attacked Player 2, their health is now decreased!")
                 print("Player 2's Health:", gladiator_2['health'])
                 break
             elif response == 'pass':
+                print("You have passed to Gladiator 2!")
                 break
             elif response == 'quit':
+                print("Hope you come to battle later!!")
+                print("***QUITTING***")
                 return
             elif response == 'heal':
                 core.heal(gladiator_1)
+                print("You have healed your health!")
                 print("Player 1's Current Health:", gladiator_1['health'])
             else:
                 print("Invalid choice, pick a valid choice to fight!!")
         if core.is_dead(gladiator_2):
             print("~~~GLADIATOR 1 WINS~~~")
+            print("Gladiator 2 has died, always in our hearts.")
             break
 
         print("*GLADIATOR 2*")
@@ -51,19 +58,25 @@ def gladiator():
             ).strip().lower()
             if response == 'attack':
                 core.attack(gladiator_2, gladiator_1)
+                print("You attacked Player 1, their health is now decreased!")
                 print("Player 1's Health:", gladiator_1['health'])
+                print("*GLADIATOR 1*")
                 break
             elif response == 'pass':
+                print("You have passed to Gladiator 2!")
                 break
             elif response == 'quit':
+                print("Hope you come to battle later!!")
+                print("***QUITTING***")
                 return
             elif response == 'heal':
                 core.heal(gladiator_1)
+                print("You have healed your health!")
                 print("Player 2's Current Health:", gladiator_2['health'])
             else:
                 print("Invalid choice, pick a valid choice to fight!!")
 
-            print("*GLADIATOR 1*")
+            #print("*GLADIATOR 1*")
 
 
 def main():
